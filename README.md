aa16e7b90c6854d124f3d9dd1f5450a0fba75a56
Kevin tan <143555909+kevintan701@users.noreply.github.com>
2024-04-15 20:43:11 -0400

    updated

---
+++ README.md
@@ -0,0 +1,5 @@
+Welcome to Kevin's Final Project - THE.LAB.701
+
+Site avalable: https://kevintan701.github.io/Final-Project-Kevin/
+
+Have a Great Day!
--- js/script.js
+++ js/script.js
@@ -13,20 +13,20 @@ closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 // ... existing code ...
 
 // Function to apply parallax effect
-function applyParallax() {
-    const scrolled = window.scrollY;
+// function applyParallax() {
+//     const scrolled = window.scrollY;
 
-    // Select elements
-    const logoWordMark = document.getElementById('logo-word-mark');
-    const logoMark = document.getElementById('logo-mark');
-    const homeContent = document.getElementById('home-content');
+//     // Select elements
+//     const logoWordMark = document.getElementById('logo-word-mark');
+//     const logoMark = document.getElementById('logo-mark');
+//     const homeContent = document.getElementById('home-content');
 
-    // Apply styles based on scroll position
-    // Change the '0.5' to adjust the speed of the parallax effect
-    logoWordMark.style.transform = 'translateY(' + scrolled * 3 + 'px)';
-    logoMark.style.transform = 'translateY(' + scrolled * 3 + 'px)';
-    homeContent.style.transform = 'translateY(' + scrolled * 3 + 'px)';
-}
+//     // Apply styles based on scroll position
+//     // Change the '0.5' to adjust the speed of the parallax effect
+//     logoWordMark.style.transform = 'translateY(' + scrolled * 3 + 'px)';
+//     logoMark.style.transform = 'translateY(' + scrolled * 3 + 'px)';
+//     homeContent.style.transform = 'translateY(' + scrolled * 3 + 'px)';
+// }
 
-// Event listener for scroll event
-window.addEventListener('scroll', applyParallax);
+// // Event listener for scroll event
+// window.addEventListener('scroll', applyParallax);
